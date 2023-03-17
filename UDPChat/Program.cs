7f7be56549611d8dcef9df9204e8058779a04890
Egorhypx <83342846+Egorhypx@users.noreply.github.com>
2023-03-17 20:22:04 +0300

    first

---
+++ UDPChat/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace UdpClientApp
+{
+    class Program
+    {
+        static string remoteAddress;
+        static int remotePort;
+        static int localPort;
+        static string username;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter your name: ");
+                username = Console.ReadLine();
+                remoteAddress = "127.0.0.1";
+                Console.Write("Enter connection port: ");
+                remotePort = Int32.Parse(Console.ReadLine());
+                Console.Write("Enter reciever port: ");
+                localPort = Int32.Parse(Console.ReadLine());
+
+                Thread receiveThread = new Thread(new ThreadStart(ReceiveMessage));
+                receiveThread.Start();
+                SendMessage();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        private static void SendMessage()
+        {
+            UdpClient sender = new UdpClient();
+            try
+            {
+                while (true)
+                {
+                    string message = Console.ReadLine();
+                    byte[] data = Encoding.Unicode.GetBytes(message);
+                    sender.Send(data, data.Length, remoteAddress, remotePort);
+                    Console.WriteLine("{0}: {1}", username, message);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                sender.Close();
+            }
+        }
+
+        private static void ReceiveMessage()
+        {
+            UdpClient receiver = new UdpClient(localPort);
+            IPEndPoint remoteIp = null;
+            try
+            {
+                while (true)
+                {
+                    byte[] data = receiver.Receive(ref remoteIp);
+                    string message = Encoding.Unicode.GetString(data);
+                    Console.WriteLine("{0}: {1}", username, message);    
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                receiver.Close();
+            }
+        }
+    }
+}
--- UDPChat/obj/Debug/net6.0/UDPChat.AssemblyInfo.cs
+++ UDPChat/obj/Debug/net6.0/UDPChat.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Lab_1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("UDPChat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Lab_1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Lab_1")]
+[assembly: System.Reflection.AssemblyProductAttribute("UDPChat")]
+[assembly: System.Reflection.AssemblyTitleAttribute("UDPChat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
